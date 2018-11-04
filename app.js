70532b54d8fda21c36611bc9e6fc600b1c88d8ac
Rahul kohli <kohlu7@gmail.com>
2018-11-04 05:24:29 +0000

    Uploading and Name changing commit

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var app = express();
 var parser = require("body-parser");
+require("dotenv").config();
+require("/.env");
 var mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASEURL);
+var db = process.env.DATABASEURL || 'mongodb://localhost/yelpcamp';
+mongoose.connect(db, { useNewUrlParser: true });
 var passport = require("passport");
 var localStrategy = require("passport-local");
 var passportLocalMongoose = require("passport-local-mongoose");
@@ -15,6 +18,7 @@ var comments = require("./models/comments");
 var campground = require("./routes/campgrounds");
 var comment = require("./routes/comments");
 var auth = require("./routes/auth");
+console.log(process.env.DATABASEURL);
 app.use(express.static(__dirname + "/public"));
 // seed();
 
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,10 +2,33 @@ var express = require("express");
 var router = express.Router();
 var camp = require("../models/campgrounds");
 
+var multer = require('multer');
+var storage = multer.diskStorage({
+    filename: function (req, file, callback) {
+        callback(null, Date.now() + file.originalname);
+    }
+});
+var imageFilter = function (req, file, cb) {
+    // accept image files only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+var upload = multer({ storage: storage, fileFilter: imageFilter })
+
+var cloudinary = require('cloudinary');
+cloudinary.config({
+    cloud_name: 'rahulkk',
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
+
 //============================================//
 //campgrounds usage routes//
 //===========================================//
 //INDEX - show all campgrounds
+
 router.get("/campgrounds", function (req, res) {
     // Get all campgrounds from DB
     camp.find({}, function (err, allCampgrounds) {
@@ -18,25 +41,24 @@ router.get("/campgrounds", function (req, res) {
     });
 });
 
-router.post("/campgrounds", function (req, res) {
-    var name = req.body.name;
-    var image = req.body.image_url;
-    var desc = req.body.description;
-    var author = {
-        id: req.user._id,
-        username: req.user.username
-    };
-    var newCamp = { name: name, image: image, description: desc, author: author };
-    camp.create(newCamp, function (err) {
-        if (err) {
-            console.log(err);
+router.post("/campgrounds", isLoggedIn, upload.single('image'), function (req, res) {
+    cloudinary.uploader.upload(req.file.path, function (result) {
+        // add cloudinary url for the image to the campground object under image property
+        req.body.campground.image = result.secure_url;
+        // add author to campground
+        req.body.campground.author = {
+            id: req.user._id,
+            username: req.user.username
         }
-        else {
-            res.redirect("/campgrounds");
-        }
-    })
+        camp.create(req.body.campground, function (err, campground) {
+            if (err) {
+                req.flash('error', "Wrong Actions");
+                return res.redirect('back');
+            }
+            res.redirect('/campgrounds/' + campground.id);
+        });
+    });
 });
-
 router.get("/campgrounds/new", isLoggedIn, function (req, res) {
 
     res.render("campgrounds/new");
